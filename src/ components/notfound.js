7d2a85fdd88a8745cc11fe62bdd4bae8b05536f5
aqilusman45 <aqilusman45@gmail.com>
2020-04-01 07:23:10 +0500

    feat: 404 page

---
+++ src/ components/notfound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div>
+        <h1>Error # 404</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
